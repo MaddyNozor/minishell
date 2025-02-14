139a74bf7ce0189afb10626bb838d88cba845fac
Mailys Riviere <mairivie@bess-f4r1s11.clusters.42paris.fr>
2025-02-14 17:26:28 +0100

    check lexing okay, slice modifie

--- source/lexing.c
+++ source/lexing.c
@@ -6,17 +6,12 @@
 /*   By: mairivie <mairivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 17:41:25 by mairivie          #+#    #+#             */
-/*   Updated: 2025/02/12 17:35:27 by mairivie         ###   ########.fr       */
+/*   Updated: 2025/02/14 17:13:38 by mairivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../include/lexing.h"
 # include "../include/init_shell.h"
-
-// # define REDIRECT_OUT 1
-// # define APPEND_OUT 2
-// # define REDIRECT_IN 3
-// # define HEREDOC 4
 
 bool    ft_is_whitespace(char c)
 {
@@ -26,32 +21,22 @@ bool    ft_is_whitespace(char c)
         return (false);
 }
 
+int ft_stop_the_count(char c)
+{
+    if (c == '<' || c == '|' || c == '>' || c == '$'
+        || ft_is_whitespace(c) == true)
+        return true;
+    return false;
+}
 
-// t_token *init_type_token_with_x_char_of_line(
-//     int type, t_token *token, int x, char *line, int i)
-
-// t_token *token_type_greater_than(char *line, int i, t_token *new_token)
-// {
-//     if  (line[i+1] == '>')
-//         new_token=init_type_token_with_x_char_of_line(
-//             APPEND_OUT, new_token, 2, line, i);
-//     else
-//         new_token=init_type_token_with_x_char_of_line(
-//             REDIRECT_OUT, new_token, 1, line, i);
-//     return(new_token);
-// }
-
-// t_token *token_type_less_than(char *line, int i, t_token *new_token)
-// {
-//     if  (line[i+1] == '<')
-//         new_token=init_type_token_with_x_char_of_line(
-//             HEREDOC, new_token, 2, line, i);
-//     else
-//         new_token=init_type_token_with_x_char_of_line(
-//             REDIRECT_IN, new_token, 1, line, i);
-//     return(new_token);
-// }
-
+int is_an_operator(int type)
+{
+    if (type == PIPE 
+        || type == REDIRECT_IN || type == REDIRECT_OUT 
+        || type == APPEND_OUT || type == HEREDOC)
+        return true;
+    return false;
+}
 
 t_token    *token_type_operators(char *line, int i, t_token *new_token)
 {
@@ -80,28 +65,6 @@ t_token    *token_type_operators(char *line, int i, t_token *new_token)
         return (NULL);
     return (new_token);
 }
-
-// t_token    *token_type_varenv(char *line, int i, t_token *new_token)
-// {
-//     int len = 1; // Par défaut, capture juste "$"
-//     int type = WORD; // Par défaut, on considère que c'est un mot
-
-//     if (line[i + 1] == '?')
-//     {
-//         len = 2;
-//         type = LAST_EXIT;
-//     }
-//     else if (line[i + 1] == '_' || ft_isalpha(line[i + 1]) == TRUE)
-//     {
-//         len = 2;
-//         type = VAR_ENV;
-//         while (ft_isalnum(line[i + len]) || line[i + len] == '_')
-//             len++; // Capture toute la variable d'environnement
-//     }
-
-//     new_token = init_type_token_with_x_char_of_line(type, new_token, len, line, i);
-//     return (new_token);
-// }
 
 t_token    *token_type_varenv(char *line, int i, t_token *new_token)
 {
@@ -132,15 +95,32 @@ t_token    *token_type_varenv(char *line, int i, t_token *new_token)
 t_token    *token_type_word(char *line, int i, t_token *new_token)
 {
     int len;
+    int quote_open;
+    char    type_of_quote;
+    int len_quote;
 
     len = 1;
-    while (line[i + len] != '<' && line[i + len] != '|' 
-        && line[i + len] != '>' && ft_is_whitespace(line[i]) == true 
-        && line[i + len] != '$')
-        len++;
+    quote_open = false;
+    while (ft_stop_the_count(line[i + len]) == false) 
+        //&& quote_open == false)
+    {
+        len_quote = 1;
+        if(line[i + len] == '\'' || line[i + len] == '\"')
+        {
+            quote_open = true;
+            type_of_quote = line[i + len];
+            while(line[i + len + len_quote] != type_of_quote)
+                len_quote++;
+            if(line[i + len + len_quote] != type_of_quote)
+                quote_open = false;    
+        }
+        len = len + len_quote;
+    }
+    if(quote_open == true)
+        return (ft_printf("rajoute une quote !\n"), NULL);
     new_token = init_type_token_with_x_char_of_line(
         WORD, new_token, len, line, i);
-    if (new_token == NULL)
+    if (new_token == NULL) //TODO: QUestion, si l'init fail, new token sera deja == NULL non ? besoin de proteger en dessous quand meme ?
         return (NULL);
     return(new_token);
 }
@@ -151,29 +131,29 @@ void	print_list(t_token *lst)
 {
     while (lst)
     {
-        printf("Token: %s, Type: %d, Prev: %p, Actual %p, Next: %p\n",
-               lst->content, lst->type, lst->prev, lst, lst->next);
+        printf("Token: %s, Type: %d\n",
+               lst->content, lst->type);
         lst = lst->next;
     }
     printf("----\n");
 }
 
-// int check_lexing(t_token *head_of_list)
-// {
-//     t_token *current_token;
+int check_lexing(t_token *head_of_list)
+{
+    t_token *current_token;
     
-//     if (head_of_list->type == PIPE)
-//         return(FAILURE);  
-//     current_token = head_of_list;
-//     while (current_token->next != NULL)
-//     {
-//         if token->type == OPERATOR
-//             if token+1->type == OPERATOR
-//                 return (FAILURE);
-//         current->token = current_token->next;
-//     }
-//     return(SUCCESS);
-// }
+    if (head_of_list->type == PIPE)
+        return(FAILURE);  
+    current_token = head_of_list;
+    while (current_token->next != NULL)
+    {
+        if (is_an_operator(current_token->type) == true)
+            if (is_an_operator(current_token->next->type) == true)
+                return (FAILURE);
+        current_token = current_token->next;
+    }
+    return(SUCCESS);
+}
 
 t_token  *lexer(char *line)
 {
@@ -197,13 +177,14 @@ t_token  *lexer(char *line)
             else
                 new_token = token_type_word(line, i, new_token);
             ft_tokadd_back(&head_of_list, new_token);
-            print_list(head_of_list);
             i = i + ft_strlen(new_token->content);
         }
     }
+    print_list(head_of_list);
     if (check_lexing(head_of_list) == FAILURE)
     {
-        head_of_list = NULL;        
-    }
+        head_of_list = NULL;
+        ft_printf("Syntax error \n----------------------------------\n");
+    }        
     return(head_of_list);
 }
