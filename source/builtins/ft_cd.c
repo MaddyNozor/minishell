dfd970b4f1c6b2a111433302f7628dad96e4c7d4
Sara Bellili <sabellil@paul-f5Br6s1.clusters.42paris.fr>
2025-03-19 17:03:38 +0100

    correction ft_cd

--- source/builtins/ft_cd.c
+++ source/builtins/ft_cd.c
@@ -6,11 +6,12 @@
 /*   By: sabellil <sabellil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 12:21:32 by sabellil          #+#    #+#             */
-/*   Updated: 2025/03/19 16:07:26 by sabellil         ###   ########.fr       */
+/*   Updated: 2025/03/19 17:03:11 by sabellil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/init_shell.h"
+
 
 bool	update_env_var(t_varenv *varenv, char *key, char *value)
 {
@@ -43,14 +44,15 @@ void	ft_update_pwd(t_data *data)
 	}
 	else
 	{
-		perror("getcwd");
+		printf("bash: getcwd: %s\n", strerror(errno));
 		data->lst_exit = 1;
 	}
 	update_exit_status(data->varenv_lst, data->lst_exit);
 }
+
 static int	handle_cd_error(t_data *data, char *path)
 {
-	fprintf(stderr, "bash: cd: %s: %s\n", path, strerror(errno));
+	printf("bash: cd: %s: %s\n", path, strerror(errno));
 	data->lst_exit = 1;
 	update_exit_status(data->varenv_lst, data->lst_exit);
 	return (1);
@@ -64,10 +66,10 @@ int	ft_cd(t_cmd *cmd, t_data *data)
 	path = NULL;
 	if (cmd->argc == 1 || !cmd->argv[1])
 	{
-		home = getenv("HOME");
+		home = get_env_value(data->varenv_lst, "HOME");
 		if (!home)
 		{
-			fprintf(stderr, "bash: cd: HOME not set\n");
+			printf("bash: cd: HOME not set\n");
 			data->lst_exit = 1;
 			update_exit_status(data->varenv_lst, data->lst_exit);
 			return (1);
@@ -84,30 +86,32 @@ int	ft_cd(t_cmd *cmd, t_data *data)
 	return (0);
 }
 
-// void	ft_cd(t_cmd *cmd, t_varenv *varenv)
+
+// int	ft_cd(t_cmd *cmd, t_data *data)
 // {
 // 	char	*path;
 // 	char	*home;
 
+// 	path = NULL;
 // 	if (cmd->argc == 1 || !cmd->argv[1])
 // 	{
 // 		home = getenv("HOME");
-// 		if (home)
+// 		if (!home)
 // 		{
-// 			if (chdir(home) == -1)
-// 				perror("cd");
-// 			else
-// 				ft_update_pwd(varenv);
+// 			printf("bash: cd: HOME not set\n");
+// 			data->lst_exit = 1;
+// 			update_exit_status(data->varenv_lst, data->lst_exit);
+// 			return (1);
 // 		}
-// 		else
-// 			fprintf(stderr, "cd: HOME not set\n");
+// 		path = home;
 // 	}
 // 	else
-// 	{
 // 		path = cmd->argv[1];
-// 		if (chdir(path) == -1)
-// 			perror("cd");
-// 		else
-// 			ft_update_pwd(varenv);
-// 	}
+// 	if (chdir(path) == -1)
+// 		return (handle_cd_error(data, path));
+// 	ft_update_pwd(data);
+// 	data->lst_exit = 0;
+// 	update_exit_status(data->varenv_lst, data->lst_exit);
+// 	return (0);
 // }
+
