f2f2a6d7a7ba042ef8bfa764571ec4579e90558d
Sara Bellili <sabellil@paul-f5Br2s1.clusters.42paris.fr>
2025-03-21 18:36:58 +0100

    Debut exit_norminette avec libft

--- source/executer/exec_pipeline_cmd.c
+++ source/executer/exec_pipeline_cmd.c
@@ -6,13 +6,12 @@
 /*   By: sabellil <sabellil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 14:04:30 by sabellil          #+#    #+#             */
-/*   Updated: 2025/03/21 18:30:45 by sabellil         ###   ########.fr       */
+/*   Updated: 2025/03/21 18:31:26 by sabellil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/init_shell.h"
  
-
 void	execute_external_cmd(t_cmd *cmd, t_data *data)
 {
 	char	*cmd_path;
